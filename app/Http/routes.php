5a3d83de9756ce75cf9b97cc9b09b37deccf0511
lerninguser663 <pv.tf.663.trial.etc@gmail.com>
2017-10-17 03:56:38 +0000

    ranking

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
